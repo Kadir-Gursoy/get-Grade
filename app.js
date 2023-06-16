99c4be2306a38665b6fa23ecdc56d7d71f22aec3
Mcclayn798 <mnkmcc9@gmail.com>
2023-06-16 23:53:55 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+
+
+
+
+
+
+
+function getGrade (s1, s2, s3) {
+    let score =  (s1+ s2+ s3) / 3;
+    return (score >= 90) ? 'A': (score >= 80) ? 'B': (score >= 70) ? 'C' : (score > 60)? 'D' : 'F';
+  }
+  console.log(score);
